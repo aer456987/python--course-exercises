359b1a7b75cf8cd4696080f1653ff8c00bcd8862
Hua Yen <aer456987@gmail.com>
2020-03-15 17:28:11 +0800

    commit_3

--- 1090315-3-products.py
+++ 1090315-3-products.py
@@ -5,10 +5,7 @@ while True:
     if name == 'q':
         break
     price = input('輸入價格: ')
-    p = []
-    p.append(name)
-    p.append(price)
-    products.append(p)
+    products.append([name,price])
 print(products)
 for product in products:
     print(product)
