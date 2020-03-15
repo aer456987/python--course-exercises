f020259380dfe89ba74247563396f59e5133ba0b
Hua Yen <aer456987@gmail.com>
2020-03-15 20:02:34 +0800

    write_text

--- 1090315-3-products.py
+++ 1090315-3-products.py
@@ -1,4 +1,4 @@
-#記帳程式1
+#記帳程式4
 products = []
 while True:
     name = input('輸入商品名稱: ')
@@ -7,5 +7,14 @@ while True:
     price = input('輸入價格: ')
     products.append([name,price])
 print(products)
-for product in products:
-    print(product)
+
+for p in products:
+    print(p)
+
+with open('text.txt', 'w') as f:
+    for p in products:
+    	f.write(p[0] + p[1] + '\n')
+
+with open('text.csv', 'w') as f:
+    for p in products:
+    	f.write(p[0] + p[1] + '\n')
